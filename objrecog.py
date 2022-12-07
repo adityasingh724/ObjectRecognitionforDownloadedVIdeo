51d483976ce615d7c1367839344534f0d2c79b97
adityasingh724 <aditya.iapple@gmail.com>
2022-12-07 17:05:17 +0530

    Add files via upload

---
+++ objrecog.py
@@ -0,0 +1,45 @@
+import cv2
+
+# Airport Object Detection System
+
+print("Developer: Aditya Singh")
+
+
+thres = 0.5 #Threshold to detect objects
+
+
+cap = cv2.VideoCapture("street2.mp4")
+classNames = []
+classFile = r'/Users/adityasingh/Downloads/sonyproject/Labels.txt'
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5,127.5,127.5))
+net.setInputSwapRB(True)
+
+while True:
+    success,img = cap.read()
+    classIds, confs, bbox = net.detect(img,confThreshold=thres)
+
+
+    print(classIds,bbox)
+
+    if len(classIds) != 0:
+
+        for classId, confidence, box in zip(classIds.flatten(),confs.flatten(),bbox):
+            cv2.rectangle(img,box,color = (255,0,0), thickness = 2)
+            cv2.putText(img,classNames[classId-1],(box[0]+10,box[1]+30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),1)
+            
+
+
+
+    cv2.imshow("Aditya Singh(Sony SSUP Recruitment Demo Project)", img)
+    cv2.waitKey(1)
